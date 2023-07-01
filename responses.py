b141d04cfe53cad2efa76df05d71c128496deb9a
Adhiraj Saha <adhirajfirst@gmail.com>
2023-07-01 20:13:11 +0530

    Added Quote command

--- responses.py
+++ responses.py
@@ -1,22 +1,32 @@
 import random
+import requests
+
 
 def get_response(message: str) -> str:
     p_message = message.lower()
 
-    if p_message == "!commands":
-        return "Type '?' before the Command to get response in DM. \nType `#hello` for greetings, \nType `#roll` to roll a dice, \nType `!help` for help, \nType `!creator` for creater's info, \nType `!commands` for list of commands"
-
-    elif p_message == "#hello":
+    if p_message == "#hello":
         return "Hi! I am WoofBot ... woof woof 🐶!!!"
     
     elif p_message == "#roll":
         return str(random.randint(1,6))
     
+    elif p_message == "#quote":
+        rs = requests.get("https://zenquotes.io/api/random")
+        quote = rs.json()[0]['q']
+        author = rs.json()[0]['a']
+        return f"{quote}  --{author}"
+
+        
+
     elif p_message == "!help":
         return "Sorry for your inconvenience, please contact my creator!!! or checkout the list of all commands using `!commands`"
     
     elif p_message == "!creator":
         return "The name of my creator is Adhiraj Saha. You can check for more details about him in his github page https://github.com/adhirajcs/"
+    
+    elif p_message == "!commands":
+        return "Type '?' before the Command to get response in DM. \nType `#hello` for greetings, \nType `#roll` to roll a dice, \nType `#quote` for a quote, \nType `!help` for help, \nType `!creator` for creator's info, \nType `!commands` for list of commands"
     
 
     return
