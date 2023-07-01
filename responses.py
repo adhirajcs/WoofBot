31570bc4b5e8e0c17014b44e6ece5491505eaf1d
Adhiraj Saha <adhirajfirst@gmail.com>
2023-07-01 22:38:49 +0530

    Added fact command

--- responses.py
+++ responses.py
@@ -22,6 +22,12 @@ def get_response(message: str) -> str:
         return f"{quote}  ~{author}"
     
 
+    elif p_message == "#fact" or p_message == "#facts":
+        rs = requests.get("https://api.chucknorris.io/jokes/random")
+        data = rs.json()
+        return data["value"]
+                            
+
     elif p_message.startswith("#weather"):
         city = p_message[8:]
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={aku.openWeatherApi}"
@@ -30,18 +36,18 @@ def get_response(message: str) -> str:
         icon = f'http://openweathermap.org/img/wn/{data["weather"][0]["icon"]}.png'
         
         return f'Temp: {data["main"]["temp"]} \nDescription: {data["weather"][0]["description"]} \n{icon}'
-                
 
-    elif p_message == "!help":
-        return "Sorry for your inconvenience, please contact my creator!!! or checkout the list of all commands using `!commands`."
+
+    elif p_message == "#help":
+        return "Sorry for your inconvenience, please contact my creator!!! or checkout the list of all commands using `#commands`."
     
     
-    elif p_message == "!creator":
+    elif p_message == "#creator":
         return "The name of my creator is Adhiraj Saha. You can check for more details about him in his github page https://github.com/adhirajcs/."
     
     
-    elif p_message == "!commands":
-        return "Type `?` before the Command to get response in DM. \nType `#hello` for greetings, \nType `#roll` to roll a dice, \nType `#quote` for a quote, \nType `#weather (city name)` for weather report of a city, \nType `!help` for help, \nType `!creator` for creator's info, \nType `!commands` for list of commands."
+    elif p_message == "#commands":
+        return "Type `?#` before the Command to get response in DM. \nType `#hello` for greetings, \nType `#roll` to roll a dice, \nType `#quote` for a quote, \nType `#fact` or `#facts` for a fact about Chuck Norris, \nType `#weather (city name)` for weather report of a city, \nType `#help` for help, \nType `#creator` for creator's info, \nType `#commands` for list of commands."
 
 
     return
