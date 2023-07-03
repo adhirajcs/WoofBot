c30b96072bb7eb1030c1c1284a3b1e6b3b24fa8c
Adhiraj Saha <adhirajfirst@gmail.com>
2023-07-03 14:04:08 +0530

    Added comments in main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# Import the 'bot' module, which contains the code for the Discord bot.
 import bot
 
+# Check if the current module is the main module.
 if __name__ == '__main__':
+
+# Run the Discord bot
     bot.run_discord_bot()
